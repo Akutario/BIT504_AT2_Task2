22defc5dbf036887065e427c778b2bd7dd1f8c37
BIT504 <Shans@192.168.2.207>
2024-04-28 22:38:50 +1200

    redraw the graphics on the UI

--- tictactoeA2/src/tictactoe/GameMain.java
+++ tictactoeA2/src/tictactoe/GameMain.java
@@ -184,7 +184,9 @@ public class GameMain extends JPanel implements MouseListener{
 			initGame();            
 		}   
 		
-		//TODO: redraw the graphics on the UI          
+		// Redraw the graphics on the UI
+		repaint();
+        
            
 	}
 		
