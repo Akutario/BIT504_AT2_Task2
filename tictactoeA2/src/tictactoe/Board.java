0d2d019b13c8649b5e6b4414bcb1b3f8b8004c09
BIT504 <Shans@192.168.2.207>
2024-04-28 20:12:09 +1200

    Check the diagonal in the other direction

--- tictactoeA2/src/tictactoe/Board.java
+++ tictactoeA2/src/tictactoe/Board.java
@@ -63,31 +63,27 @@ public class Board {
         return false;
     }
 	
-	/**
-	 * Draws the grid (rows then columns) using constant sizes, then call on the
-	 * Cells to paint themselves into the grid
-	 */
-	public void paint(Graphics g) {
-		//draw the grid
-		g.setColor(Color.gray);
-		for (int row = 1; row < GameMain.ROWS; ++row) {          
-			g.fillRoundRect(0, GameMain.CELL_SIZE * row - GRID_WIDHT_HALF,                
-					GameMain.CANVAS_WIDTH - 1, GRID_WIDTH,                
-					GRID_WIDTH, GRID_WIDTH);       
-			}
-		for (int col = 1; col < GameMain.COLS; ++col) {          
-			g.fillRoundRect(GameMain.CELL_SIZE * col - GRID_WIDHT_HALF, 0,                
-					GRID_WIDTH, GameMain.CANVAS_HEIGHT - 1,                
-					GRID_WIDTH, GRID_WIDTH);
-		}
-		
-		//Draw the cells
-		for (int row = 0; row < GameMain.ROWS; ++row) {          
-			for (int col = 0; col < GameMain.COLS; ++col) {  
-				cells[row][col].paint(g);
-			}
-		}
-	}
-	
+    /**
+     * Draws the grid (rows then columns) using constant sizes, then call on the
+     * Cells to paint themselves into the grid
+     */
+    public void paint(Graphics g) {
+        // draw the grid
+        g.setColor(Color.gray);
+        for (int row = 1; row < GameMain.ROWS; ++row) {
+            g.fillRoundRect(0, GameMain.CELL_SIZE * row - GRID_WIDHT_HALF, GameMain.CANVAS_WIDTH - 1, GRID_WIDTH,
+                    GRID_WIDTH);
+        }
+        for (int col = 1; col < GameMain.COLS; ++col) {
+            g.fillRoundRect(GameMain.CELL_SIZE * col - GRID_WIDHT_HALF, 0, GRID_WIDTH, GameMain.CANVAS_HEIGHT - 1,
+                    GRID_WIDTH);
+        }
 
+        // Draw the cells
+        for (int row = 0; row < GameMain.ROWS; ++row) {
+            for (int col = 0; col < GameMain.COLS; ++col) {
+                cells[row][col].paint(g);
+            }
+        }
+    }
 }
