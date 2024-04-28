ca66330fd5139d55ddad61a4fd4c1c10bbebc696
BIT504 <Shans@192.168.2.207>
2024-04-28 21:25:17 +1200

    Set the value of content to Empty (Its an enum)

--- tictactoeA2/src/tictactoe/Cell.java
+++ tictactoeA2/src/tictactoe/Cell.java
@@ -51,10 +51,8 @@ public class Cell {
 	
 	/** Set this cell's content to EMPTY */
 	public void clear() {
-		
-		// TODO: Set the value of content to Empty (Remember this is an enum)
-
-		
+	    // Set the value of content to Empty (Remember this is an enum)
+	    content = Player.EMPTY;
 	}
-		
 }
+		
